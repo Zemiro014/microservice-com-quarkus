9c3b5c73d23aecd7820a1eb4d77e4360ee5c2cca
Zemiro014 <zemiro014@gmail.com>
2021-06-06 12:06:17 -0300

    Test do EndPoint PUT no Cadastro

--- cadastro/src/test/java/RestauranteResourceTest.java
+++ cadastro/src/test/java/RestauranteResourceTest.java
@@ -7,13 +7,17 @@ import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.jeronimo.ifood.cadastro.CadastroTestLifecycleManager;
+import com.github.jeronimo.ifood.cadastro.entity.Restaurante;
 
 import org.approvaltests.Approvals;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -24,11 +28,34 @@ public class RestauranteResourceTest {
     @Test
     @DataSet("restaurantes-cenario-1.yml")
     public void testBuscarRestaurantes() {
-        String resultado = RestAssured.given()
+        String resultado = given()
                 .when().get("/restaurantes")
                 .then()
                 .statusCode(Status.OK.getStatusCode())
                 .extract().asString();
        Approvals.verifyJson(resultado);
+    }
+
+    private RequestSpecification given(){
+        return RestAssured.given().contentType(ContentType.JSON);
+    }
+    // Exemplo de um teste de PUT
+    @Test
+    @DataSet("restaurantes-cenario-1.yml")
+    public void testAlterarRestaurante(){
+        Restaurante dto = new Restaurante();
+        dto.nome = "novoNome";
+        Long parameterValue = 123L;
+        given()
+            .with().pathParam("id", parameterValue)
+            .body(dto)
+            .when().put("/restaurantes/{id}")
+            .then()
+            .statusCode(Status.NO_CONTENT.getStatusCode())
+            .extract().asString();
+        Restaurante findById = Restaurante.findById(parameterValue);
+
+        // Poderia testar todos os outros atributos
+        Assert.assertEquals(dto.nome, findById.nome);
     }
 }

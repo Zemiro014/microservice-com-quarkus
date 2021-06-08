82d39dd2e94f822a5306467ac8666fd1a8798431
jeronimo kulandissa <onsultoria.jeronimo@grupoivory.com.br>
2021-06-07 21:42:14 -0300

    Adicionando Keycloak ao docker-compose, Protegendo endpoints e atualizando OpenAPI

--- cadastro/src/main/java/com/github/jeronimo/ifood/cadastro/web/resources/RestauranteResource.java
+++ cadastro/src/main/java/com/github/jeronimo/ifood/cadastro/web/resources/RestauranteResource.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -33,15 +34,24 @@ import com.github.jeronimo.ifood.cadastro.entity.Prato;
 import com.github.jeronimo.ifood.cadastro.entity.Restaurante;
 import com.github.jeronimo.ifood.cadastro.infra.ConstraintViolationResponse;
 
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/restaurantes")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Tag(name = "restaurante")
+@RolesAllowed("proprietario")
+@SecurityScheme(securitySchemeName = "ifood-oauth", 
+type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token")))
+@SecurityRequirement(name = "ifood-oauth", scopes = {})
 public class RestauranteResource {
 
     @Inject

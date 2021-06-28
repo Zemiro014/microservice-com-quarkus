d77219c97a9a3e427dedffc3a69171e49e5d2177
jeronimo kulandissa <consultoria.jeronimo@grupoivory.com.br>
2021-06-27 21:06:23 -0300

    Microservico Pedido, configurando MongoDB, entidades, e Kafka (no docker-compose)

---
+++ pedido/src/main/java/com/github/jeronimo/ifood/pedido/entity/Localizacao.java
@@ -0,0 +1,18 @@
+public class Localizacao {
+
+    private Double latitude;
+    private Double longitude;
+    
+    public Double getLatitude() {
+        return latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+}
